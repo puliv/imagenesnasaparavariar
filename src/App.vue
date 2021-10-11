7cbf9e31658aa46056cdd144bd2046202f535b81
puliv <pverapavez@gmail.com>
2021-10-11 01:12:48 -0300

    ya se obtienen las imagenes segun el dia que se seleccione

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="app">
     <div class="home">
-      <v-calendar mode="date" :model-config="modelConfig" v-model="date" />
-      <img alt="" :src="home_img" />
+      <div class="selected-date">
+        {{ home_title !== null ? home_title : "" }}
+      </div>
+      <v-calendar :attributes="attributes" @dayclick="onDayClick" :max-date="today" />
+      <img alt="" v-if="home_video == null" :src="home_img" />
+      <!-- <video :src="home_video" v-if="home_video !== null" /> -->
       <div class="text">
         {{ home_text }}
       </div>
@@ -23,43 +27,70 @@ export default {
     return {
       home_img: null,
       home_text: null,
-      date: null,
-      modelConfig: {
-        type: "number",
-        mask: "YYYY-MM-DD" // Uses 'iso' if missing
-      }
+      home_video: null,
+      home_title: null,
+      dateSelected: null,
+      days: [],
+      today: new Date()
     };
+  },
+  computed: {
+    dates() {
+      return this.days.map((day) => day.date);
+    },
+    attributes() {
+      return this.dates.map((date) => ({
+        highlight: true,
+        dates: date
+      }));
+    }
   },
   methods: {
     getData() {
       var api =
         "https://api.nasa.gov/planetary/apod?api_key=rsLmxghvWRUQWRVjQhKN6Jg750ipPyTEnIjQ0Xrj";
       this.axios.get(api).then((response) => {
-        console.log(response.data);
+        // console.log(response.data);
+        var data = response.data;
 
-        this.home_img = response.data.url;
-        this.home_text = response.data.explanation;
+        if (data.media_type == "video") {
+          var vid = data.url.split(":");
+          this.home_video = "http:" + vid[1];
+          // console.log(this.home_video);
+          this.home_title = data.title;
+        }
+        if (data.media_type == "image") {
+          this.home_img = data.url;
+          this.home_video = null;
+          this.home_title = data.title;
+        }
+
+        this.home_text = data.explanation;
       });
     },
     getImageByDay(day) {
-      // var year = date.year;
-      // var month = date.month <= 9 ? "0" + date.month : date.month;
-      // var day = date.day <= 9 ? "0" + date.day : date.day;
-      console.log(day);
-
       var api_key = "rsLmxghvWRUQWRVjQhKN6Jg750ipPyTEnIjQ0Xrj";
-      var date = day.id;
+      var date = day.id.split("-");
       console.log(date);
 
-      var api = `https://api.nasa.gov/planetary/apod?api_key=${api_key}&date=${date}`;
+      var api = `https://api.nasa.gov/planetary/apod?api_key=${api_key}&date=${
+        date[0] + "-" + date[1] + "-" + date[2]
+      }`;
+      console.log("api", api);
 
       this.axios.get(api).then((response) => {
-        console.log(response.data);
+        // console.log(response.data);
+        var _data = response.data;
+        if (_data.media_type == "image") {
+          this.home_img = _data.url;
+          this.home_title = _data.title;
+          this.home_video = null;
+          this.home_text = _data.explanation;
+        }
       });
     },
     onDayClick(day) {
       const idx = this.days.findIndex((d) => d.id === day.id);
-      this.getImageByDay(day);
       if (idx >= 0) {
         this.days.splice(idx, 1);
       } else {
@@ -68,16 +99,12 @@ export default {
           date: day.date
         });
       }
+      this.getImageByDay(day);
     }
   },
   mounted() {
     this.getData();
   }
-  // watch: {
-  //   date: function (val) {
-  //     console.log(val);
-  //   }
-  // }
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from "./App.vue";
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import VCalendar from 'v-calendar';
+import VueVideoPlayer from 'vue-video-player'
 
 Vue.config.productionTip = false;
 
 Vue.use(VueAxios, axios)
 Vue.use(VCalendar)
+Vue.use(VueVideoPlayer)
 
 new Vue({
   render: (h) => h(App),
